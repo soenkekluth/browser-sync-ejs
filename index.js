b8fcb253eff2e6c5a980fe1b556de27fceceeb3d
Sönke Kluth <soenke.kluth@gmail.com>
2014-09-04 15:54:06 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var url = require('url');
+var path = require('path');
+var fs = require('fs');
+var ejs = require('ejs');
+var _ = require('lodash');
+
+
+module.exports = function(opt) {
+
+  opt = opt || {};
+  var ext = opt.ext || 'ejs';
+  var ejsOpt = opt.ejs || {};
+  var baseDir = opt.baseDir || __dirname;
+
+  return function(req, res, next) {
+    var file = req.url === '/' ? ('/index.' + ext) : req.url;
+    var pathname = path.join(baseDir, url.parse(file).pathname);
+
+    if (pathname.indexOf(ext) > -1 && fs.existsSync(pathname)) {
+      var urlParts = url.parse(req.url, true);
+      var contents = fs.readFileSync(pathname).toString();
+      contents = ejs.render(contents, ejsOpt);
+
+      if (opt.browserSync) {
+        contents = contents.replace(/<\/head>/, '<script async src="//' + req.headers.host + '/browser-sync-client.1.3.6.js"></script></head>');
+      }
+
+      res.write(contents);
+
+      res.end();
+    } else {
+      next();
+    }
+  };
+};
